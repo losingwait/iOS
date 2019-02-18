a4260f333a81fec748b2cfbb293c4a5338205546
Mike JS. Choi <mkchoi212@icloud.com>
2019-02-18 13:22:19 -0600

    Add tab icons

---
+++ LosingWait/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  LosingWait
+//
+//  Created by Mike Choi on 2/18/19.
+//  Copyright © 2019 Mike JS. Choi. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
