0a004ceebfcda08a7e74671cf947f9811ec0a6ef
Mike JS. Choi <mkchoi212@icloud.com>
2019-02-18 12:39:27 -0600

    Initial commit

---
+++ LosingWait/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LosingWait
+//
+//  Created by Mike Choi on 2/18/19.
+//  Copyright © 2019 Mike JS. Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# LosingWait
+
+This repo is for the iOS application portion of Texas A&M University CE Senior Design, Spring 2019.
+
+# License
+
+MIT License
+
