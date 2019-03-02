83168333d2fbe060f0e9882d21bf95acdfc106ff
Mike JS. Choi <mkchoi212@icloud.com>
2019-03-02 14:26:09 -0600

    Add workout media player

--- LosingWait/Workouts/ActiveWorkoutViewController.swift
+++ LosingWait/Workouts/ActiveWorkoutViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ActiveWorkoutViewController: UIViewController {
     
@@ -19,14 +20,62 @@ class ActiveWorkoutViewController: UIViewController {
     @IBOutlet weak var alternatesTableView: UITableView!
     
     var workout: Workout?
+    var player: AVPlayer?
+    var playerItem: AVPlayerItem?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configurePopupItem()
+        initializeVideoPlayerWithVideo()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if player != nil {
+            player?.play()
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        player?.pause()
+    }
+    
+    func configurePopupItem() {
         let back = UIBarButtonItem(image: #imageLiteral(resourceName: "prev-mini"), style: .plain, target: nil, action: nil)
         let next = UIBarButtonItem(image: #imageLiteral(resourceName: "next-mini"), style: .plain, target: nil, action: nil)
         popupItem.image = #imageLiteral(resourceName: "arnold-chest")
         popupItem.rightBarButtonItems = [back, next]
         popupItem.title = "Hello World"
+    }
+    
+    func initializeVideoPlayerWithVideo(){
+        videoView.backgroundColor = .lightGray
+        
+        playerItem =  AVPlayerItem(url: URL(string: "https://videos.bodybuilding.com/video/mp4/70000/71792l.mp4")!)
+        player = AVPlayer(playerItem: playerItem)
+        player?.allowsExternalPlayback = true
+
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = videoView.bounds
+        layer.videoGravity = .resizeAspectFill
+        videoView.layer.addSublayer(layer)
+        
+        player?.currentItem?.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions(rawValue: 0), context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        if let currentPlayer = player,
+            let playerObject = object as? AVPlayerItem,
+            playerObject == currentPlayer.currentItem,
+            keyPath == "status" {
+            
+            if currentPlayer.currentItem!.status == .readyToPlay {
+                currentPlayer.play()
+                currentPlayer.rate = 1.0
+            }
+        }
     }
 }
