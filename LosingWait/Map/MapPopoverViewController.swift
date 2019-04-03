9f9d675f32033c9271f893a737069c0f84065e2e
Karsen Keith <karsen.keith@gmail.com>
2019-04-03 12:08:06 -0500

    Added queueButton press functionality to join queue with API call

--- LosingWait/Map/MapPopoverViewController.swift
+++ LosingWait/Map/MapPopoverViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class MapPopoverViewController: UIViewController {
     
@@ -22,6 +23,7 @@ class MapPopoverViewController: UIViewController {
     static let identifier = "MapPopoverViewController"
     
     var machineName: String?
+    var thisMachineGroup: MachineGroup?
     
     var _occupied: Bool?
     var occupied: Bool {
@@ -31,6 +33,18 @@ class MapPopoverViewController: UIViewController {
             statusLabel.textColor = newValue ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) : #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
         } get {
             return _occupied ?? false
+        }
+    }
+    
+    // setter and getter for toggling the get in queue button
+    var _inQueue: Bool?
+    var inQueue: Bool {
+        set {
+            _inQueue = newValue
+            queueButton.setTitle(newValue ? "Leave queue" : "Get in queue", for: UIButton.State.normal)
+            queueButton.setTitleColor(newValue ? #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1) : #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1), for: UIButton.State.normal)
+        } get {
+            return _inQueue ?? false
         }
     }
     
@@ -44,6 +58,48 @@ class MapPopoverViewController: UIViewController {
     }
     
     @IBAction func queuePressed(_ sender: UIButton) {
+        // send update request
+        updateUserToQueue(alreadyInQueue: self.inQueue, completion: { ok in
+            // toggle queue button
+            self.inQueue = !self.inQueue
+            
+            // reconfigure so that you can see accurate queue status
+            self.configure(for: self.machineName)
+        })
+        
+    }
+    
+    func updateUserToQueue(alreadyInQueue: Bool, completion: @escaping (Bool) -> ()) -> () {
+        let endpoint = URL(string: "https://losing-wait.herokuapp.com/queue")!
+        
+        guard let id: String = UserDefaults.standard.string(forKey: "id"),
+            let group_id: String = thisMachineGroup?.id else {
+            fatalError("User not logged in. Need User ID")
+        }
+        
+        let action: String = alreadyInQueue ? "remove" : "add"
+        
+        let parameters: Parameters = ["_id": group_id, "user_id": id, "action": action]
+
+        let headers: HTTPHeaders = [
+            "Accept": "application/json"
+        ]
+
+        Alamofire.request(endpoint, method: .post, parameters: parameters, encoding: URLEncoding.default, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success:
+                print("Successfully added/removed user from queue")
+            case .failure(let error):
+                BannerNotification.fatalError(msg: "Could not access server").show()
+                print(error)
+            }
+            completion(true)
+
+            let code = response.response?.statusCode
+            if(code == 401) {
+                print("User not added/removed from queue")
+            }
+        }
     }
     
     func configure(for machineName: String?) {
@@ -59,11 +115,17 @@ class MapPopoverViewController: UIViewController {
                 self.queueLabel.text = "?"
                 return
             }
+            self.thisMachineGroup = targetGroup
             
             if let queue = targetGroup.queue {
                 self.queueLabel.text = "\(queue.count)"
+                guard let id: String = UserDefaults.standard.string(forKey: "id") else {
+                    fatalError("User not logged in. Need User ID")
+                }
+                self.inQueue = queue.contains(id) ? true : false
             } else {
                 self.queueLabel.text = "0"
+                self.inQueue = false
             }
         }
         
